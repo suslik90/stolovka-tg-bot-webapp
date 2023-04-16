837ad75733354d183d90e0e3e495736eb02f069e
Roman Smekalov <r.smekalov@teleguard.com>
2023-04-16 23:53:52 +0300

    feat: modified routing, added Forbidden View, added event click on Menu Item

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,35 @@
 </template>
 
 <script setup>
-  import { RouterView } from "vue-router";
+  import { RouterView, useRouter } from "vue-router";
   import { useTheme } from "vuetify";
   import { tg } from "@/utils/telegram-sdk";
 
   const theme = useTheme();
+  const router = useRouter();
+
+  const isProd = import.meta.env.PROD;
+
+  if (isProd) {
+    if (tg.initData.length === 0) {
+      router.replace({ name: "forbidden" });
+    }
+  }
 
   tg.onEvent("themeChanged", function () {
-    theme.global.name.value = theme.global.name.value == "customDarkTheme"
-      ? "customLightTheme"
-      : "customDarkTheme";
+    theme.global.name.value =
+      theme.global.name.value == "customDarkTheme"
+        ? "customLightTheme"
+        : "customDarkTheme";
   });
 
   if (tg.colorScheme === "light") {
-    theme.global.name.value = "customLightTheme";    
+    theme.global.name.value = "customLightTheme";
   } else {
     theme.global.name.value = "customDarkTheme";
   }
+
+  tg.BackButton.onClick(() => {
+    router.back();
+  });
 </script>
---
+++ src/views/ForbiddenView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex align-center justify-center forbidden-message px-4">
+    Данное приложение предназначено для использования только в качестве Telegram Bot
+  </div>
+</template>
+
+<style lang="scss">
+  .forbidden-message {
+    width: 100%;
+    height: 100vh;
+    color: rgb(var(--v-theme-head-title-color));
+    text-align: center;
+  }
+</style>
